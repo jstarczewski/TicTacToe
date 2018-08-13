b985d8bbf5e004ae1fdac7233c2a76bccfd17c12
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-13 11:35:47 +0200

    PlayerTest class created

---
+++ src/com/jstarczewski/Tests/PlayerTest.java
@@ -0,0 +1,32 @@
+package com.jstarczewski.Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void setTacAsFigure() {
+    }
+
+    @Test
+    public void setTicAsFigure() {
+    }
+
+    @Test
+    public void createFigure() {
+    }
+
+    @Test
+    public void setMoveIndex() {
+    }
+
+    @Test
+    public void getMoveIndex() {
+    }
+
+    @Test
+    public void getFiguresName() {
+    }
+}
